daa12229157c1ada56f183c403d554ce919a5e5c
James Duong <james@beforeyoubid.com.au>
2022-11-13 17:53:29 +1100

    Finished Gatsby Tutorial Part 4

---
+++ src/components/layout.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+  siteTitle,
+} from "./layout.module.css";
+import { Link, useStaticQuery, graphql } from "gatsby";
+
+const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <div className={container}>
+      <nav>
+        <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
